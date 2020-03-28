62b50c82f882038c1cc3d57855486b93de9699d3
wgallop <wgallop@deloitte.com>
2020-03-28 13:24:51 -0400

    added apollo server with resolvers

---
+++ pages/api/resolvers.js
@@ -0,0 +1,31 @@
+import Nomics from 'nomics';
+
+const nomics =  ({
+    apiKey: process.env.API_KEY
+})
+
+const resolvers = {
+    Query: {
+        getCoins: async (_, args) => {
+            // fetchs all coins from nomics
+            const coins = await nomics.currenciesTicker()
+
+            // let's only return the top 10
+            return coins.slice(0, 10)
+        },
+
+        getCoinDetails: async (_, args) => {
+            const { coinId, interval } = args;
+
+            // fetch coin from nomics for specified interval
+            const fetchCoinDetails = await nomics.currenciesTicker({
+                ids: [`${coinId}`.toUpperCase()],
+                interval: [`${interval}`]
+            })
+
+            return fetchCoinDetails[0][`${interval}`];
+        }
+    }
+}
+
+export default resolvers
