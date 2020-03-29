511ac03d7e14d6a363df866c88ef2a70a433491f
wgallop <wgallop@deloitte.com>
2020-03-29 18:35:42 -0400

    added Layout and Header components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-import Head from 'next/head'
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { Loader, Divider } from 'semantic-ui-react'
+import CoinsTable from '../components/Index/CoinsTable'
+import { GET_COINS_QUERY } from '../utils/queries'
 
-const Home = () => (
-  <div className="container">
-    <h1>Homepage</h1>
-  </div>
-)
+export default () => {
+  const { data, loading, error } = useQuery(GET_COINS_QUERY, {
+    fetchPolicy: 'network-only',
+    pollInterval: 5000,
+  })
 
-export default Home
+  return (
+    <div>
+      <h1>Top 30 Cryptocurrencies</h1>
+      <Divider />
+      {error ? <h1>Error Fetching Coin Data!</h1> : null}
+      {loading ? (
+        <Loader active inline="centered" content="Loading" />
+      ) : (
+          <CoinsTable coins={data.getCoins} />
+        )}
+    </div>
+  )
+}
