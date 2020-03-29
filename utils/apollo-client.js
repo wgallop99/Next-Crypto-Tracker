1bf3f1da8d96258296753a6f29d4475dc4444cec
wgallop <wgallop@deloitte.com>
2020-03-29 18:17:56 -0400

    added apollo server and queries

---
+++ utils/apollo-client.js
@@ -0,0 +1,25 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import withApollo from "next-width-apollo";
+import fetch from "isomorphic-unfetch";
+import { createHttpLink } from "apollo-link-http";
+
+const GRAPHQL_URL = "/api/graphql";
+
+const link = createHttpLink({
+  fetch, // Switches between unfetch & node-fetch for client & server.
+  uri: GRAPHQL_URL
+});
+
+export default withApollo(
+  // You can get headers and ctx (context) from the callback params
+  // e.g. ({ headers, ctx, initialState })
+
+  ({ initialState }) =>
+    new ApolloClient({
+      link,
+      cache: new InMemoryCache()
+        // rehydrate the cache using the inital data passed from the server:
+        .restore(initialState || {})
+    })
+);
